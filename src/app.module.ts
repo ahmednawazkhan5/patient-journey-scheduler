636f895686d0227e5894a6746a69d7294085974e
Ahmed Nawaz Khan <ahmed.nawaz@tenpearls.com>
2025-07-31 01:03:44 +0500

    clean up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { JourneyModule } from './modules/journey.module';
 import configurations from './config';
@@ -22,7 +20,5 @@ import configurations from './config';
     DatabaseModule,
     JourneyModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
